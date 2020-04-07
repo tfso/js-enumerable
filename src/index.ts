0559b6db7487104665bfd28b0a3358ade77f79a4
lostfields <nicolai.gjaerum@gmail.com>
2020-04-07 18:35:12 +0200

    reducing webpack bundle-size

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
+import 'core-js/modules/es.symbol.async-iterator'
+import 'core-js/modules/es.symbol'
+
 export { Enumerable, IEnumerable } from './linq'
 export { Repository, IRepository } from './repository'
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,17 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        lib: ['@babel/polyfill', './src/index.ts'],
-        mocha: ['@babel/polyfill', './src/test/enumerable/primary.ts', './src/test/enumerable/record.ts']
+        lib: [
+            'core-js/modules/es.symbol.async-iterator',
+            'core-js/modules/es.symbol',
+            './src/index.ts'
+        ],
+        mocha: [
+            'core-js/modules/es.symbol.async-iterator',
+            'core-js/modules/es.symbol',
+            './src/test/enumerable/primary.ts', 
+            './src/test/enumerable/record.ts'
+        ]
     },
     optimization: {
         minimize: true
@@ -18,23 +27,40 @@ module.exports = {
         rules: [
             {
                 test: /\.(js|jsx|tsx|ts)$/,
-                exclude: /node_modules/,
+                exclude: [
+                    /node_modules/
+                ],
                 loader: 'babel-loader',
                 options: {
                     presets: [
-                        '@babel/preset-env',
+                        ['@babel/preset-env', {
+                            useBuiltIns: 'usage',
+                            corejs: 3,
+                            targets: { chrome: 58, ie: 11 }
+                        }],
                         '@babel/preset-typescript'
                     ],
                     plugins: [
                         '@babel/proposal-class-properties',
-                        //'@babel/proposal-object-rest-spread'
+                        //'@babel/plugin-transform-async-to-generator',
+                        //'@babel/proposal-object-rest-spread',
+                        //'@babel/plugin-syntax-dynamic-import',
+                        [
+                            '@babel/plugin-transform-runtime',
+                            {
+                                corejs: false,
+                                helpers: true,
+                                regenerator: true,
+                                useESModules: true,
+                            },
+                        ]
                     ]
                 }
             }
         ]
     },
     resolve: {
-        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+        extensions: ['.js', '.jsx', '.tsx', '.ts'],
     },
     plugins: [
         new webpack.ProvidePlugin({
